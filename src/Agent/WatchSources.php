0aec914e65b0e8b38240bc72a2dd9811d8ae0ab9
Baptiste Langlade <baptouuuu@gmail.com>
2019-03-13 19:33:29 +0100

    send a message to a process when sources are modified

---
+++ src/Agent/WatchSources.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\LabStation\Agent;
+
+use Innmind\LabStation\{
+    Agent,
+    Protocol,
+    Activity,
+    Activity\Type,
+};
+use Innmind\FileWatch\Watch;
+use Innmind\IPC\{
+    IPC,
+    Process\Name,
+};
+use Innmind\Url\{
+    PathInterface,
+    Path,
+};
+
+final class WatchSources implements Agent
+{
+    private $protocol;
+    private $watch;
+    private $ipc;
+    private $monitor;
+
+    public function __construct(
+        Protocol $protocol,
+        Watch $watch,
+        IPC $ipc,
+        Name $monitor
+    ) {
+        $this->protocol = $protocol;
+        $this->watch = $watch;
+        $this->ipc = $ipc;
+        $this->monitor = $monitor;
+    }
+
+    public function __invoke(PathInterface $project): void
+    {
+        $src = new Path($project.'/src');
+
+        ($this->watch)($src)(function() {
+            $monitor = $this->ipc->get($this->monitor);
+            $monitor->send(
+                $this->protocol->encode(
+                    new Activity(Type::sourcesModified(), [])
+                )
+            );
+            $monitor->close();
+        });
+    }
+}
