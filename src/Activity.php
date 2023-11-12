c1b3153895b185a6b71b24bbc9c3b9526ce9ae0a
Baptiste Langlade <baptouuuu@gmail.com>
2023-11-12 14:37:25 +0100

    remove deadcode

--- src/Activity.php
+++ src/Activity.php
@@ -10,26 +10,4 @@ enum Activity
     case fixturesModified;
     case propertiesModified;
     case start;
-
-    public static function of(string $type): self
-    {
-        return match ($type) {
-            'sourcesModified' => self::sourcesModified,
-            'testsModified' => self::testsModified,
-            'fixturesModified' => self::fixturesModified,
-            'propertiesModified' => self::propertiesModified,
-            'start' => self::start,
-        };
-    }
-
-    public function toString(): string
-    {
-        return match ($this) {
-            self::sourcesModified => 'sourcesModified',
-            self::testsModified => 'testsModified',
-            self::fixturesModified => 'fixturesModified',
-            self::propertiesModified => 'propertiesModified',
-            self::start => 'start',
-        };
-    }
 }
